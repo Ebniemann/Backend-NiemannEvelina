01c535804669f5d7ed8f281462a7031cc95b59b5
Evelina Niemann <evelinaniemann@gmail.com>
2023-11-06 03:22:19 -0300

    Router and app.js

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const productRouter = require("./router/products.router");
+
+const PORT = 8080;
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/products", productRouter);
+
+app.get("/", (req, res) => {
+  res.setHeader("Content-Type", "text/html");
+  res.status(200).send("<h2>Bienvenidos!</h2>");
+});
+
+const server = app.listen(PORT, () => {
+  console.log("hola");
+});
